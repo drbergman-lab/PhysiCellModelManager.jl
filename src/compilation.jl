5a523487fff2ce024c6ca6b1fe7fdc14d87d3ff4
Daniel Bergman <danielrbergman@gmail.com>
2025-07-28 20:54:12 -0400

    allow debug in dl of librr stuff

--- src/compilation.jl
+++ src/compilation.jl
@@ -326,7 +326,20 @@ function prepareLibRoadRunner()
     librr_file = joinpath(librr_dir, "include", "rr", "C", "rrc_api.h")
     if !isfile(librr_file)
         python = Sys.iswindows() ? "python" : "python3"
-        cd(() -> run(pipeline(`$(python) $(joinpath(".", "beta", "setup_libroadrunner.py"))`; stdout=devnull, stderr=devnull)), physicellDir())
+        show_output = begin
+            if !haskey(ENV, "JULIA_DEBUG")
+                return false
+            end
+            debug_modules = split(ENV["JULIA_DEBUG"], ",")
+            if "PhysiCellModelManager" in debug_modules
+                return true
+            end
+            return false
+        end
+        cd(() -> run(pipeline(`$(python) $(joinpath(".", "beta", "setup_libroadrunner.py"))`;
+                     stdout=show_output ? stdout : devnull,
+                     stderr=show_output ? stderr : devnull)),
+           physicellDir())
         @assert isfile(librr_file) "libRoadrunner was not downloaded properly."
 
         #! remove the downloaded binary (I would think the script would handle this, but it does not)
